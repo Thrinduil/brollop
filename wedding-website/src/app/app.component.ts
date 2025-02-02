ec46cbf3b00c9a47ccf7384e74a52f0de9170f7d
Fredrik Söderquist <fredrik@fredriksoderquist.se>
2025-02-02 17:09:15 +0100

    Lägg till where.component.ts

--- wedding-website/src/app/app.component.ts
+++ wedding-website/src/app/app.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {WeddingInfoComponent} from "./wedding-info/wedding-info.component";
 import {WhatComponent} from "./whats-up/what.component";
+import {WhereComponent} from "./where/where.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, WeddingInfoComponent, WhatComponent],
+  imports: [CommonModule, WeddingInfoComponent, WhatComponent, WhereComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ wedding-website/src/app/where/where.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {CardUi} from "../ui/card.ui";
+
+@Component({
+  selector: 'app-where',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardTitle,
+    CardUi
+  ],
+  template: `
+    <app-card>
+      <mat-card>
+        <mat-card-header>
+          <mat-card-title>Var?</mat-card-title>
+        </mat-card-header>
+        <mat-card-content>
+          <p>
+            <a href="https://spa.hogbygard.se/">Högby gård!</a>
+          </p>
+          <p>
+            Matilda har alltid älskat spa och Fredrik har fantastiskt nog också börjat förstå tjusningen med spamiljön
+            även om badkruke-tendens finns! Då vi besökt ett antal span under våra år tillsammans känns det helt rätt
+            att även fira vårt bröllop på ett. Och just Högby gård var vi på ett par månader innan Ronja kom.
+          </p>
+          <p>
+            Högby gård är flera hundra år gammalt och ligger mysigt till utanför Borensberg.
+          </p>
+          <p>
+            Om du liksom bruden saknar lokalsinne eller bara tycker att det är rimligt med GPS finns
+            <a href="https://spa.hogbygard.se/kontakta-oss/">exakt position här</a>.
+          </p>
+          <p>
+            Det finns gott om parkeringsplatser och man kan även åka buss till Högbylyckan,
+            ca 500 meter från Högby gård.
+          </p>
+        </mat-card-content>
+      </mat-card>
+    </app-card>
+  `,
+  styles: ``
+})
+export class WhereComponent {
+
+}
